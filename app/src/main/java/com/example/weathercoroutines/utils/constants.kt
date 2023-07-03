8e9d87531d508a3226bcc53117b59e98484e059f
Max Shapira <83203304+JustMax7CB@users.noreply.github.com>
2023-07-03 17:06:20 +0300

    Delete constants.kt

--- app/src/main/java/com/example/weathercoroutines/utils/constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weathercoroutines.utils
-
-object Constants {
-    val API_KEY: String = "fb0219174a93496aa88112734230307"
-    val WeatherAPI_BaseURL: String = "https://api.weatherapi.com/v1/current.json?key=$API_KEY"
-
-
-}
